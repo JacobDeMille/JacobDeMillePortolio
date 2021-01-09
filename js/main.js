6eab9003603ae2162e171072409f828c37fb2d1e
JacobDeMille <63323359+JacobDeMille@users.noreply.github.com>
2021-01-09 16:47:22 -0700

    added more to js

--- js/main.js
+++ js/main.js
@@ -9,24 +9,34 @@ const links = [{
     url: "W02/"
 }];
 
+const ol = document.querySelector("ol");
 
+for (const item of links) {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.setAttribute('href', item.url);
+    a.textContent = item.label;
+    li.appendChild(a);
 
-function createOL(array) {
-    // Create list element
-    var list = document.getElementById("list");
-
-    for (var i = 0; i < array.length; i++) {
-        // Create list item
-        var li = document.createElement("li");
-        // Set the Contents
-        li.appendChild(document.createTextNode(array[i]));
-        // Add to the list
-        list.appendChild(li);
-    }
-
-    return list;
+    ol.appendChild(li);
 }
 
-document.getElementById('list').appendChild(createOL(links));
+// function createOL(array) {
+//     // Create list element
+//     var list = document.getElementById("list");
 
-createOL(links);
+//     for (var i = 0; i < array.length; i++) {
+//         // Create list item
+//         var li = document.createElement("li");
+//         // Set the Contents
+//         li.appendChild(document.createTextNode(array[i]));
+//         // Add to the list
+//         list.appendChild(li);
+//     }
+
+//     return list;
+// }
+
+// document.getElementById('list').appendChild(createOL(links));
+
+// createOL(links);
