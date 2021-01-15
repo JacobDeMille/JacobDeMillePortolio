b6a1947781d0ba6c1ac823966ca1f87410e2b162
JacobDeMille <63323359+JacobDeMille@users.noreply.github.com>
2021-01-15 15:11:39 -0700

    Added js for FizzBuzz activity

--- W02/main.js
+++ W02/main.js
@@ -13,3 +13,16 @@ do {
     bottles--;
 } while (bottles > 0)
 
+// FizzBuzz activity from eloqentjavascript
+// This is my solution to the activity 
+for(i = 1; i <= 100; i++) {
+	if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+	}else if(i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
