c4750916c5c3580ca8b1395ad3a6003ef0ac7220
JacobDeMille <63323359+JacobDeMille@users.noreply.github.com>
2021-01-15 10:04:23 -0700

    added code for the Quiz Ninja project

--- quizninja/main.js
+++ quizninja/main.js
@@ -1,1 +1,6 @@
+// Code for chapter 1
 alert('Welcome to Quiz Ninja!');
+// Code from chapter 2
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
