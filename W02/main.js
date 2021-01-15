bb29066d6e946c4a780533fbaa4ccd82aa3c8316
JacobDeMille <63323359+JacobDeMille@users.noreply.github.com>
2021-01-15 12:06:42 -0700

    added notes and code for quiz ninja

---
+++ W02/main.js
@@ -0,0 +1,15 @@
+/* Example of a while loop
+let bottles = 10;
+while (bottles > 0){
+    console.log(`There were ${bottles} green bottles, hanging on a wall. And if one green bottle should accidentally fall, there'd be ${bottles-1} green bottles hanging on the wall`);
+    bottles--;
+}
+*/
+
+// Example of a do while loop
+let bottles = 10;
+do {
+    console.log(`There were ${bottles} green bottles, hanging on a wall. And if one green bottle should accidentally fall, there'd be ${bottles-1} green bottles hanging on the wall`);
+    bottles--;
+} while (bottles > 0)
+
