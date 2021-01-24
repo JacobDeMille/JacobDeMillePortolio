777d88ee70db5e3f0d1fa96f41d867b52872ab02
JacobDeMille <jacob.demille@icloud.com>
2021-01-23 17:01:03 -0700

    added code for quizninja game

--- coding-exercises/quizninja/main.js
+++ coding-exercises/quizninja/main.js
@@ -64,7 +64,20 @@ const quiz = [
     {name: "Iron Man", realName: "Tony Stark"},
     {name: "Hulk", realName: "Bruce Banner"},
     {name: "Thor", realName: "Thor"},
-]
+];
+
+const view = {
+    score: document.querySelector("score strong"),
+    question: document.getElementById("question"),
+    result: document.getElementById("result"),
+    info: document.getElementById("info"),
+    render(target, content, attributes) {
+        for(const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    }
+}
 
 const game = {
     start(quiz){
@@ -79,21 +92,24 @@ const game = {
     },
     ask(){
         const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question,question);
         const response = prompt(question);
         this.check(response);
     },
     check(response){
         const answer = this.question.realName;
         if(response === answer) {
+            view.render(view.result,"Correct!",{'class':'correct'});
             alert('Correct!');
             this.score++;
+            view.render(view.score,this.score);
         } else {
+            view.render(view.result,`Wrong! The correct ansewr is ${answer}`,{'class':'wrong'});
             alert(`Wrong! The correct answer is ${answer}`);
         }
     },
     gameOver() {
-        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's': ''}`);
-        document.getElementById("score").innerHTML = `Score ${this.score}`;
+        view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's': ''}`);
     }
 }
 
