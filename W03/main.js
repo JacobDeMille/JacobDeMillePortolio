281be5e04b6f9b3805880f0a39c26fcda85e495c
JacobDeMille <jacob.demille@icloud.com>
2021-01-23 18:17:17 -0700

    more notes and code for quiz ninja project

--- W03/main.js
+++ W03/main.js
@@ -13,3 +13,32 @@ const mouseParagraph = document.getElementById('mouse');
 mouseParagraph.addEventListener('mouseover', highlight);
 mouseParagraph.addEventListener('mouseout', highlight);
 mouseParagraph.addEventListener('mousemove', () =>  console.log('You Moved!') );
+addEventListener('keydown',highlight);
+addEventListener('keyup', (event) => console.log(`You stopped pressing the key on ${new Date}`));
+addEventListener('keydown', (event) => console.log(`You pressed the ${event.key} character`));
+addEventListener('keydown', (event) => {
+    if (event.key === 'c' && event.ctrlKey) {
+            console.log('Action canceled!');
+        }
+    });
+
+    const onceParagraph = document.getElementById('once');
+    onceParagraph.addEventListener('click', remove);
+    function remove(event) {
+        console.log('Enjoy this while it lasts!');
+        onceParagraph.style.backgroundColor = 'pink';
+        onceParagraph.removeEventListener('click',remove);
+    }
+
+    const brokenLink = document.getElementById('broken');
+brokenLink.addEventListener('click',(event) => {
+    event.preventDefault();
+    console.log('Broken Link!');
+});
+
+ulElement = document.getElementById('list');
+liElement = document.querySelector('#list li');
+ulElement.addEventListener('click', (event) =>
+console.log('Clicked on ul') );
+liElement.addEventListener('click', (event) =>
+console.log('Clicked on li') );
--- coding-exercises/quizninja/main.js
+++ coding-exercises/quizninja/main.js
@@ -71,11 +71,18 @@ const view = {
     question: document.getElementById("question"),
     result: document.getElementById("result"),
     info: document.getElementById("info"),
+    start: document.getElementById('start'),
     render(target, content, attributes) {
         for(const key in attributes) {
             target.setAttribute(key, attributes[key]);
         }
         target.innerHTML = content;
+    },
+    show(element) {
+        element.style.display = 'block';
+    },
+    hide(element) {
+        element.style.display = 'none';
     }
 }
 
@@ -89,6 +96,8 @@ const game = {
             this.ask();
         }
         this.gameOver();
+        view.hide(view.start);
+        view.show(view.start);
     },
     ask(){
         const question = `What is ${this.question.name}'s real name?`;
@@ -113,4 +122,4 @@ const game = {
     }
 }
 
-game.start(quiz);
+view.start.addEventListener('click', () => game.start(quiz),false);
