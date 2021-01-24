ecb7a9e94b1c04ac6b10c6b6508223bd7c5b771b
JacobDeMille <jacob.demille@icloud.com>
2021-01-23 17:37:42 -0700

    Some changes

--- W03/main.js
+++ W03/main.js
@@ -1,4 +1,15 @@
-function doSomething() {
-    console.log('Something Happened!');
+const clickParagraph = document.getElementById('click');
+clickParagraph.addEventListener('click',() => console.log('click') );
+clickParagraph.addEventListener('mousedown',() => console.log('down') );
+clickParagraph.addEventListener('mouseup',() => console.log('up') );
+
+const dblclickParagraph = document.getElementById('dblclick');
+dblclickParagraph.addEventListener('dblclick', highlight);
+function highlight(event){
+    event.target.classList.toggle('highlight');
 }
-addEventListener('click', doSomething);
+
+const mouseParagraph = document.getElementById('mouse');
+mouseParagraph.addEventListener('mouseover', highlight);
+mouseParagraph.addEventListener('mouseout', highlight);
+mouseParagraph.addEventListener('mousemove', () =>  console.log('You Moved!') );
