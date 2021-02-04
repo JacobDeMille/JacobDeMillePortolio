068c2a6571efb4a67d42e567b99f1da505ea876b
JacobDeMille <jacob.demille@icloud.com>
2021-02-04 13:14:36 -0700

    another

--- todo/main.js
+++ todo/main.js
@@ -1,2 +1,5 @@
 // All of the code for the todo list application goes here
 
+// todo lost data source: localStorage
+todo = { id : timestamp, content: string, completed: bool };
+toDoList = [toDo];
