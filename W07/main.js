bcaa1d55829a01905638014072beae2b5c598960
JacobDeMille <jacob.demille@icloud.com>
2021-02-20 15:19:42 -0700

    code code code

--- W07/main.js
+++ W07/main.js
@@ -2,8 +2,8 @@ const textButton = document.getElementById("number");
 const apiButton = document.getElementById("chuck");
 const outputDiv = document.getElementById("output");
 
-const textURL = "http://numbersapi.com.random";
-const apiUTL = "https://api.chucknorris.io/jokes/random";
+const textURL = 'http://numbersapi.com/random';
+const apiURL = 'https://api.chucknorris.io/jokes/random';
 
 textButton.addEventListener('click', () => {
   fetch(textURL)
@@ -15,7 +15,20 @@ textButton.addEventListener('click', () => {
     throw Error(response.statusText);
   })
   .then( response => response.text() )
-  .then( text => outputDiv.innerHTML = text )
+  .then( text => outputDiv.innerText = text )
   .catch( error => console.log('There was an error:', error))
 },false);
 
+apiButton.addEventListener('click', () => {
+  fetch(apiURL)
+  .then( response => {
+    outputDiv.innerHTML = 'Waiting for resonse...';
+  if(response.ok) {
+    return response;
+  }
+  throw Error(response.statusText);
+  })
+  .then(response => response.json())
+  .then(data => outputDiv.innerText = data.value)
+  .catch(error => console.log('There was an error:', error))
+},false);
