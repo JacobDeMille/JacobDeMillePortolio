9954bc570d82313d715e182eef176dd355053b1d
JacobDeMille <jacob.demille@icloud.com>
2021-04-01 13:36:43 -0600

    clkf

--- final-project/local-storage.js
+++ final-project/local-storage.js
@@ -1,15 +1,18 @@
-function loadStory() {
-  let storyName = document.getElementById("name_input").value;
-  let storyHTML = localStorage.getItem(storyName);
-  document.getElementById("story_editor").value = storyHTML;
-}
-function saveStory() {
-  let storyName = document.getElementById("name_input").value;
-  let storyHTML = document.getElementById("story_editor").value;
-  localStorage.setItem(storyName, storyHTML);
-}
-function displayStory() {
-  console.log(story_editor)
-  let storyHTML = document.getElementById("story_editor").value;
-  document.getElementById("story_display").innerHTML = storyHTML;
-}
+// code from the local storage activity durring week 4
+
+// function loadStory() {
+//   let storyName = document.getElementById("name_input").value;
+//   let storyHTML = localStorage.getItem(storyName);
+//   document.getElementById("story_editor").value = storyHTML;
+// }
+// function saveStory() {
+//   let storyName = document.getElementById("name_input").value;
+//   let storyHTML = document.getElementById("story_editor").value;
+//   localStorage.setItem(storyName, storyHTML);
+// }
+// function displayStory() {
+//   console.log(story_editor)
+//   let storyHTML = document.getElementById("story_editor").value;
+//   document.getElementById("story_display").innerHTML = storyHTML;
+// }
+
