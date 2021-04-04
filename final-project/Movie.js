03c22384517a615f6f014246c31a9485b1cc59f1
JacobDeMille <jacob.demille@icloud.com>
2021-04-03 21:12:18 -0600

    I want to be done

--- final-project/Movie.js
+++ final-project/Movie.js
@@ -30,8 +30,9 @@ export default class Movie {
     const page = getRandomPage(numberOfPages, min);
     // console.log("")
     // console.log(`The page number for this query will be: ${page}`)
+    
 
-    const ratingList = `https://api.themoviedb.org/3/discover/movie?api_key=651dd06cfce5f626304367d5d45cb689&with_genres=${genre}&certification_country=US&certification.lte=${rating}&certification.gte=${rating}&language=en-US&include_adult=false&include_video=true&primary_release_date.gte=${startYear}-01-01&primary_release_date.lte${endYear}-12-31&page=${page}`;
+    const ratingList = `https://api.themoviedb.org/3/discover/movie?api_key=651dd06cfce5f626304367d5d45cb689&with_genres=${genre}&certification_country=US&certification.lte=${rating}&certification.gte=${rating}&language=en-US&include_adult=false&include_video=true&primary_release_date.gte=${endYear}-01-01&primary_release_date.lte=${startYear}-12-31&page=${page}`;
     this._ratings = getJSON(ratingList);
     console.log(this._ratings);
     return this._ratings;
